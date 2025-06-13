aa552b698dafa75a92b38c513b644a10592e80f8
Helen <lenagreidon@mail.ru>
2025-06-13 12:39:47 +0300

    added requests for user

--- nest-backend/src/user/user.controller.ts
+++ nest-backend/src/user/user.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
+import { Controller, Get, Body, UsePipes, ValidationPipe, HttpCode, Put } from "@nestjs/common";
 import { UserService } from "./user.service";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
+import { CurrentUser } from "src/auth/decorators/user.decorator";
+import { Auth } from "src/auth/decorators/auth.decorator";
+import { UserDto } from "./dto/user.dto";
 
-@Controller("user")
+@Controller("user/profile")
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  /*  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
-  }
-
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  @Auth()
+  async profile(@CurrentUser("id") id: string) {
+    return this.userService.getProfile(id);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Put()
+  @Auth()
+  async updateProfile(@CurrentUser("id") id: string, @Body() dto: UserDto) {
+    return this.userService.update(id, dto);
   }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
-  } */
 }
