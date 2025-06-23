3c3039970357e8406fd15fbce332fc9e238bb648
Helen <lenagreidon@mail.ru>
2025-06-23 12:39:39 +0300

    created checkbox and button

---
+++ next-frontend/src/components/ui/buttons/Button.tsx
@@ -0,0 +1,17 @@
+import cn from "clsx";
+import type { ButtonHTMLAttributes, PropsWithChildren } from "react";
+
+type TypeButton = ButtonHTMLAttributes<HTMLButtonElement>;
+
+export function Button({ children, className, ...rest }: PropsWithChildren<TypeButton>) {
+	return (
+		<button
+			className={cn(
+				"linear rounded-lg bg-transparent vorder border-primary py-2 px-7 text-base fonct-medium text-white transition hover:bg-primary active:bg-brand-700",
+				className,
+			)}
+		>
+			{children}
+		</button>
+	);
+}
