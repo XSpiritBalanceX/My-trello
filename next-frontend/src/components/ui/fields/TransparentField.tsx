d898a961387142c215ba92f3f452ef35fa982f2d
Helen <lenagreidon@mail.ru>
2025-07-02 15:30:45 +0300

    created list row for task panel

---
+++ next-frontend/src/components/ui/fields/TransparentField.tsx
@@ -0,0 +1,16 @@
+import cn from "clsx";
+import { type InputHTMLAttributes, forwardRef } from "react";
+
+type TTransparentField = InputHTMLAttributes<HTMLInputElement>;
+
+export const TransparentField = forwardRef<HTMLInputElement, TTransparentField>(({ className, ...rest }, ref) => {
+	return (
+		<input
+			className={cn("bg-transparent border-none focus:outline-0 focus:shadow-transparent w-full", className)}
+			ref={ref}
+			{...rest}
+		/>
+	);
+});
+
+TransparentField.displayName = "TransparentField";
